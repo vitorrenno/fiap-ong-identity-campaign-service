ddd6ba9a7a8c83a791dbc6f15e5d717b779aa57c
Eduarda Matias <evcm2003@gmail.com>
2026-05-18 15:18:06 -0300

    chore: remove commented code

--- src/IdentityCampaign.Api/Program.cs
+++ src/IdentityCampaign.Api/Program.cs
@@ -65,7 +65,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 app.UseAuthorization();
 
 DotNetRuntimeStatsBuilder.Default().StartCollecting();
