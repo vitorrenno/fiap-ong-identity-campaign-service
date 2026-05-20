69e822e0f524df0d3749ab754a5b16fc2b53a948
Eduarda Matias <evcm2003@gmail.com>
2026-05-20 12:22:07 -0300

    feat: publish and consume donation events using RabbitMQ

--- src/IdentityCampaign.Application/Features/Donation/CreateDonation/CreateDonationCommandHandler.cs
+++ src/IdentityCampaign.Application/Features/Donation/CreateDonation/CreateDonationCommandHandler.cs
@@ -1,9 +1,6 @@
 using AutoMapper;
 using IdentityCampaign.Application.Abstractions;
-using IdentityCampaign.Application.Features.Campaigns.CreateCampaign;
-using IdentityCampaign.Domain.Entities;
 using MediatR;
-
 namespace IdentityCampaign.Application.Features.Donation.CreateDonation
 {
     public class CreateDonationCommandHandler : IRequestHandler<CreateDonationCommand, CreateDonationResponse>
@@ -20,12 +17,11 @@ namespace IdentityCampaign.Application.Features.Donation.CreateDonation
 
         public async Task<CreateDonationResponse> Handle(CreateDonationCommand request, CancellationToken cancellationToken)
         {
-            var donation = new Domain.Entities.Donation(request.vAmount,request.IdCampaign,request.IdUser);
+            Domain.Entities.Donation donation = new Domain.Entities.Donation(request.vAmount,request.IdCampaign,request.IdUser);
 
             //Verifica se a campanha existe antes de doar para a mesma.
-            var campaign = await _campaignRepository.GetByIdAsync(request.IdCampaign, cancellationToken);
-            if (campaign == null)
-                throw new KeyNotFoundException($"Campanha com ID {request.IdCampaign} não encontrado.");
+            Domain.Entities.Campaign? campaign = await _campaignRepository.GetByIdAsync(request.IdCampaign, cancellationToken)
+                ?? throw new KeyNotFoundException($"Campanha com ID {request.IdCampaign} não encontrado.");
 
             //Aplicar chamada para o service aqui
             await _donationRepository.AddAsync(donation, cancellationToken);
